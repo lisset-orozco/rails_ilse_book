b9e2620e33552b9f7fc85c89524113b6f3223009
lisset-orozco <liss612@gmail.com>
2019-05-22 13:46:53 -0500

    create administrator with the seed, and modify view so they can start and close session

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.create(
+  name: 'Luisa Gonzalez',
+  email: 'luisa@emailg.com',
+  password: 'luisa123',
+  password_confirmation: 'luisa123'
+)
