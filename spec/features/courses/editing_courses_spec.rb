345835afeac904d2c42091228f5d92c10d1e640e
lisset-orozco <liss612@gmail.com>
2019-05-22 15:08:44 -0500

    only admins can edit courses

--- spec/features/courses/editing_courses_spec.rb
+++ spec/features/courses/editing_courses_spec.rb
@@ -4,6 +4,15 @@ require 'rails_helper'
 
 RSpec.feature 'Users can edit courses', type: :feature do
   let!(:course) { FactoryBot.create(:course) }
+  
+  before do
+    admin = FactoryBot.create(:admin)
+    visit '/'
+    click_link 'Step in'
+    fill_in 'admin_email', with: admin.email
+    fill_in 'admin_password', with: admin.password
+    click_button 'Log in'
+  end
 
   scenario 'with valid attributes' do
     visit '/'
@@ -12,5 +21,14 @@ RSpec.feature 'Users can edit courses', type: :feature do
     fill_in 'Name', with: 'Advanced Mathematics'
     click_button 'Save'
     expect(page).to have_content 'The course has been updated.'
+  end
+
+  scenario 'with invalid attributes' do
+    visit '/'
+    click_link course.name
+    click_link 'Edit'
+    fill_in 'Name', with: ''
+    click_button 'Save'
+    expect(page).to have_content 'The course has not been updated.'
   end
 end
